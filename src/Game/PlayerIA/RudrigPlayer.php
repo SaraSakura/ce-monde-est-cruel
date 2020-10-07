a96dc27cac355ffa631b32a1e0f2d2e144ee8421
sara Mellouk <saramellouk@MacBook-Pro-de-sara.local>
2020-10-07 21:22:56 +0200

    [stratégy] strategy modified with stats

--- src/Game/PlayerIA/RudrigPlayer.php
+++ src/Game/PlayerIA/RudrigPlayer.php
@@ -17,19 +17,25 @@ class RudrigPlayer extends Player
 
     public function getChoice()
     {
-       $round = 0;
 
-       if ($round > 0)
+       if ($this->result->getLastChoiceFor($this->opponentSide) === parent::rockChoice() )
+           return parent::paperChoice();
+       if ($this->result->getLastChoiceFor($this->opponentSide) === parent::paperChoice())
+           return parent::scissorsChoice();
+       if ($this->result->getLastChoiceFor($this->opponentSide) === parent::scissorsChoice())
+           return parent::rockChoice();
+
+       if ($round > 3)
        {
-            if ($this->result->getLastChoiceFor($this->opponentSide) === parent::rockChoice() )
-                 return parent::paperChoice();
-            if ($this->result->getLastChoiceFor($this->opponentSide) === parent::paperChoice())
-                 return parent::scissorsChoice();
-            if ($this->result->getLastChoiceFor($this->opponentSide) === parent::scissorsChoice())
-                 return parent::rockChoice();
+            $arr = $this->result->getStatsFor($this->opponentSide);
+            if ($arr[2] > $arr[3] && $arr[2] > $arr[4])
+                return parent::rockChoice();
+            if ($arr[3] > $arr[2] && $arr[3] > $arr[4])
+                return parent::scissorsChoice();
+            if ($arr[4] > $arr[2] && $arr[4] > $arr[3])
+                return parent::paperChoice();
        }
 
-       $round++;
        return parent::rockChoice();
     }
 };
